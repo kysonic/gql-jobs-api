7c8e5d6a0457767ff49f8fd8ee16b88ab8cc87e2
Anton Miroshnochenko <anton.miroshnichenko@uula.com>
2021-12-30 13:20:51 +0300

    feat: file uploades

--- src/resolvers/mutation/file/uploadFile.ts
+++ src/resolvers/mutation/file/uploadFile.ts
@@ -5,12 +5,8 @@ import { finished } from 'stream/promises';
 export default async (parent: undefined, { file }: any) => {
   const { createReadStream, filename, mimetype, encoding } = await file;
 
-  // Invoking the `createReadStream` will return a Readable Stream.
-  // See https://nodejs.org/api/stream.html#stream_readable_streams
   const readStream = createReadStream();
 
-  // This is purely for demonstration purposes and will overwrite the
-  // local-file-output.txt in the current working directory on EACH upload.
   const out = fs.createWriteStream(`./public/uploads/${filename}`);
   readStream.pipe(out);
   await finished(out);
